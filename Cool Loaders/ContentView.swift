65e0faae645a500e626264c185490d6f6c49ba1a
realvjy <realvjy@gmail.com>
2023-12-20 23:10:42 +0530

    Initial Commit

---
+++ Cool Loaders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cool Loaders
+//
+//  Created by vijay verma on 20/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cool Loaders/Cool_LoadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cool_LoadersApp.swift
+//  Cool Loaders
+//
+//  Created by vijay verma on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Cool_LoadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
